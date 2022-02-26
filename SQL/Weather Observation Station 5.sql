0cfc36341aee16f0024cb4cc04c832bfdd8ec934
Mostafa Khairy Abo Dahab <87584678+mostafa-khairy@users.noreply.github.com>
2022-02-26 17:43:18 +0200

    Create Weather Observation Station 5.sql

---
+++ SQL/Weather Observation Station 5.sql
@@ -0,0 +1,15 @@
+/*
+Query the two cities in STATION with the shortest and longest CITY names,
+as well as their respective lengths (i.e.: number of characters in the name). 
+If there is more than one smallest or largest city, choose the one that comes first when ordered alphabetically.
+*/
+
+select city , length(city) from station 
+where length(city) = (select min(length(city)) from station) 
+order by city 
+limit 1 ;
+ 
+select city , length(city) from station 
+where length(city) = (select max(length(city)) from station) 
+order by city desc
+limit 1 ;
