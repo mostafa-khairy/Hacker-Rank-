bef3056afa8250057c4517bd8ecf5336fb8f0be8
Mostafa Khairy Abo Dahab <87584678+mostafa-khairy@users.noreply.github.com>
2022-06-23 12:29:38 +0200

    Update Python If-Else.py

--- Python/Python If-Else.py
+++ Python/Python If-Else.py
@@ -3,3 +3,20 @@
         print('Not Weird')
     else :
          print('Weird'
+               
+# or 
+               
+               
+ 
+n = int(input().strip())
+
+if n % 2 != 0 :
+    print("Weird")
+if n % 2 == 0 : 
+    if n in range(2,5):
+        print("Not Weird")    
+    elif n in range(6,21) :
+        print("Weird")
+    else :
+        print("Not Weird")    
+               
